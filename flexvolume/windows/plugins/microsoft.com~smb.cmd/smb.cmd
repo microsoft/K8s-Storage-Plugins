a569046e2b147c72f587402db2feb232ac0d81df
Nick Maliwacki <nmaliwa@microsoft.com>
2018-04-24 11:44:11 -0700

    Use duplicated code intead of symlink

---
+++ flexvolume/windows/plugins/microsoft.com~smb.cmd/smb.cmd
@@ -0,0 +1,24 @@
+@echo off
+:::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::
+:::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::
+
+setlocal enabledelayedexpansion
+
+:::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::
+:: Get the drive, path, and file name of this file, minus the .CMD extension
+set scriptname=%~dpn0
+
+:::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::
+:: using environment variables to avoid escaping pains
+set flexvolume_command=%1
+set flexvolume_folder=%2
+
+:: Not trying to parse the json as it ended up being hard
+:: script just gets the executed command line off the parent process (this script)
+:: set flexvolume_json=%3
+:: set flexvolume_all=%*
+
+:::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::
+:: launch powershell
+PowerShell.exe -NoLogo -Sta -NoProfile -ExecutionPolicy "ByPass" -Command "%scriptname%.ps1"
+goto :eof
